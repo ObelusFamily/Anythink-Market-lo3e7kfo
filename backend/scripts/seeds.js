bef10dfb597d8543197d4328a412b815abc915d7
menachem-r <a716827@gmail.com>
2022-12-22 00:28:04 +0000

    test

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,5 +1,16 @@
 require("dotenv").config();
 mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
-require("./models/Item");
+require("../models/User");
+require("../models/Item");
+require("../models/Comment");
 var Item = mongoose.model("Item");
+const i = new Item({title:"title"})
+console.log('-------------------------------------b')
+i.save().then(()=>{
+    
+    console.log('-------------------------------------a')
+    mongoose.connection.close()
+}).catch((e)=>{console.error('faild db seed',e)
+mongoose.connection.close()
+})
